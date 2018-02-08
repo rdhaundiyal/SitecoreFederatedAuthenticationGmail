0d68af5098e060aeb6c6f33b373098d4a9e7a2a4
rdhaundiyal@hotmail.com <rdhaundiyal@hotmail.com>
2018-02-08 11:38:35 +0400

    Add project files.

---
+++ ClassLibrary1/Processor/GmailIdentityProcessor.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Owin.Security.Google;
+using Owin;
+using Sitecore.Diagnostics;
+using Sitecore.Owin.Authentication.Configuration;
+using Sitecore.Owin.Authentication.Pipelines.IdentityProviders;
+using Sitecore.Owin.Authentication.Services;
+using System.Security.Claims;
+using System.Web;
+
+
+namespace SitecoreGmailAuth.Processor
+{
+   public class GmailIdentityProcessor:IdentityProvidersProcessor
+    {
+        public GmailIdentityProcessor(FederatedAuthenticationConfiguration federatedAuthenticationConfiguration) :
+            base(federatedAuthenticationConfiguration)
+        {
+        }
+
+        /// <summary>
+        /// Identityprovider name. Has to match the configuration
+        /// </summary>
+        protected override string IdentityProviderName
+        {
+            get { return "Google"; }
+        }
+
+        protected override void ProcessCore(IdentityProvidersArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            IdentityProvider identityProvider = this.GetIdentityProvider();
+          
+            var clientId = Sitecore.Configuration.Settings.GetSetting("FedAuth.Google.ClientId");
+            var clientSecret = Sitecore.Configuration.Settings.GetSetting("FedAuth.Google.ClientSecret");
+            var domain = Sitecore.Configuration.Settings.GetSetting("FedAuth.Google.Domain");
+
+            var provider = new GoogleOAuth2AuthenticationProvider
+            {
+                OnAuthenticated = (context) =>
+                {
+                    // transform all claims
+                    ClaimsIdentity identity = context.Identity;
+                    foreach (Transformation current in identityProvider.Transformations)
+                    {
+                        current.Transform(identity, new TransformationContext(FederatedAuthenticationConfiguration, identityProvider));
+                    }
+                    return System.Threading.Tasks.Task.FromResult(0);
+                },
+
+                OnReturnEndpoint = (context) =>
+                {
+                    // xsrf validation
+                    if (context.Request.Query["state"] != null && context.Request.Query["state"].Contains("xsrf="))
+                    {
+                        var state = HttpUtility.ParseQueryString(context.Request.Query["state"]);
+                        //todo: do something with it.
+                        //change response status to 401 in case of forgery
+                    }
+
+                    return System.Threading.Tasks.Task.FromResult(0);
+                }
+            };
+
+            args.App.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = clientId,
+                ClientSecret =clientSecret,
+                Provider = provider
+
+            });
+
+
+        }
+    }
+}
---
+++ ClassLibrary1/UserBuilder/CustomUserBuilder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Sitecore.Diagnostics;
+using Sitecore.Owin.Authentication.Configuration;
+using Sitecore.Owin.Authentication.Identity;
+using Sitecore.Owin.Authentication.Services;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SitecoreGmailAuth.UserBuilder
+{
+    public class CustomUserBuilder : ExternalUserBuilder
+    {
+        [Obsolete("Use DefaultExternalUserBuilder(bool isPersistentUser) instead.")]
+        public CustomUserBuilder()
+          : this(true)
+        {
+        }
+
+        public CustomUserBuilder(bool isPersistentUser)
+        {
+            this.IsPersistentUser = isPersistentUser;
+        }
+
+        public CustomUserBuilder(string isPersistentUser)
+          : this(bool.Parse(isPersistentUser))
+        {
+        }
+
+        public bool IsPersistentUser { get; set; }
+
+        public override ApplicationUser BuildUser(UserManager<ApplicationUser> userManager, ExternalLoginInfo externalLoginInfo)
+        {
+            return new ApplicationUser(this.CreateUniqueUserName(userManager, externalLoginInfo))
+            {
+                IsVirtual = !this.IsPersistentUser,
+
+            };
+        }
+
+        [SuppressMessage("Microsoft.Naming", "CA1726:UsePreferredTerms", MessageId = "Login")]
+        protected virtual string CreateUniqueUserName(UserManager<ApplicationUser> userManager, ExternalLoginInfo externalLoginInfo)
+        {
+            Assert.ArgumentNotNull((object)userManager, nameof(userManager));
+            Assert.ArgumentNotNull((object)externalLoginInfo, nameof(externalLoginInfo));
+            IdentityProvider identityProvider = this.FederatedAuthenticationConfiguration.GetIdentityProvider(externalLoginInfo.ExternalIdentity);
+            if (identityProvider == null)
+                throw new InvalidOperationException("Unable to retrieve identity provider for given identity");
+            string domain = identityProvider.Domain;
+            return domain + "\\" + externalLoginInfo.Email;
+
+        }
+    }
+}
